581de2d3630c622f3eacf9f786bc3172f04e6a3a
Amiran-Kiladze <amirani.kiladze99@gmail.com>
2023-10-13 11:28:28 +0400

    feature: location picker component which has two buttons one is location if clicked user is asked for permission for finding out location if confirmed user locations coordinates are figured out but map preview does not work sinced it required biling google account

---
+++ components/Places/LocationPicker.jsx
@@ -0,0 +1,70 @@
+import { View, StyleSheet, Alert } from 'react-native';
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location';
+
+import OutLinedButton from '../UI/OutlinedButton';
+import { Colors } from '../../constants/colors';
+
+const LocationPicker = () => {
+  const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+  const verifyPermissions = async () => {
+    if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions!',
+        'You need to grant camera permissions to use this app'
+      );
+      return false;
+    }
+
+    return true;
+  };
+
+  const getLocationHandler = async () => {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+    const location = await getCurrentPositionAsync();
+    console.log('heya', location);
+  };
+
+  const pickOnMapHandler = () => {};
+  return (
+    <View>
+      <View style={styles.mapPreview}></View>
+      <View style={styles.actions}>
+        <OutLinedButton icon="location" onPress={getLocationHandler}>
+          Locate User
+        </OutLinedButton>
+        <OutLinedButton icon="map" onPress={pickOnMapHandler}>
+          Pick on Map
+        </OutLinedButton>
+      </View>
+    </View>
+  );
+};
+
+export default LocationPicker;
+
+const styles = StyleSheet.create({
+  mapPreview: {
+    width: '100%',
+    height: 200,
+    marginVertical: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+});
