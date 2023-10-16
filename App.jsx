1a6c657e6d7709335051da02e11073f6a69dbd0e
Amiran-Kiladze <amirani.kiladze99@gmail.com>
2023-10-16 11:27:47 +0400

    feature: register map component in a router

--- App.jsx
+++ App.jsx
@@ -6,6 +6,7 @@ import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import IconButton from './components/UI/IconButton';
 import { Colors } from './constants/colors';
+import Map from './screens/Map';
 
 const Stack = createNativeStackNavigator();
 
@@ -47,6 +48,7 @@ export default function App() {
               title: 'Add a new place',
             }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
